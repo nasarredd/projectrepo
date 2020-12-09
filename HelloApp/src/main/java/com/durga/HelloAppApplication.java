bb3c7be2c019de5aa0e911be089d8cb665a93b85
nasarredd <75736719+nasarredd@users.noreply.github.com>
2020-12-09 20:11:04 +0530

    Update HelloAppApplication.java

--- HelloApp/src/main/java/com/durga/HelloAppApplication.java
+++ HelloApp/src/main/java/com/durga/HelloAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloAppApplication {
 
-	public static void main(String[] arg) {
-		SpringApplication.run(HelloAppApplication.class, arg);
+	public static void main(String[] args) {
+		SpringApplication.run(HelloAppApplication.class, args);
 	}
 
 }
